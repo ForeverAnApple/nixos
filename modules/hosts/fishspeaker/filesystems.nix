d1500f1c05904fd8d6f78c7c181c7b331fe265db
ForeverAnApple <kinbd8@gmail.com>
2025-10-16 20:43:42 -0700

    Migrate hardware configurations.

---
+++ modules/hosts/fishspeaker/filesystems.nix
@@ -0,0 +1,22 @@
+{
+  flake.modules.nixos.host_fishspeaker = { ... } : {
+    
+    fileSystems."/" =
+      { device = "/dev/disk/by-uuid/105ec169-951f-44be-a5a1-4d02a358a960";
+        fsType = "ext4";
+      };
+
+    boot.initrd.luks.devices."luks-d342d12f-cb15-45eb-8704-ca58e3238abc".device = "/dev/disk/by-uuid/d342d12f-cb15-45eb-8704-ca58e3238abc";
+
+    fileSystems."/boot" =
+      { device = "/dev/disk/by-uuid/AA9E-51F5";
+        fsType = "vfat";
+        options = [ "fmask=0077" "dmask=0077" ];
+      };
+
+    swapDevices =
+      [ { device = "/dev/disk/by-uuid/5f9d9d02-845c-4857-b771-a453a32c6f9a"; }
+      ];
+
+  };
+}
---
+++ modules/hosts/fishspeaker/hardware.nix
@@ -0,0 +1,40 @@
+{
+  flake.modules.nixos.host_fishspeaker =
+    {
+      config,
+      lib,
+      modulesPath,
+      pkgs,
+      ...
+    }:
+    {
+      imports = [
+        (modulesPath + "/installer/scan/not-detected.nix")
+      ];
+
+      boot = {
+        # Use latest linux kernel
+	kernelPackages = pkgs.linuxPackages_latest;
+
+	initrd = {
+	  availableKernelModules = [
+	    "xhci_pci"
+	    "thunderbolt"
+	    "nvme"
+	    "usb_storage"
+	    "sd_mod"
+	  ];
+
+	  kernelModules = [ ];
+	};
+
+	kernelModules = [ "kvm-intel" ];
+	extraModulePackages = [ ];
+      };
+
+      networking.useDHCP = lib.mkDefault true;
+
+      nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+      hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+    };
+}
