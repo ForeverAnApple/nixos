1ea77495daa09859115bbde5b2c2f346121b640b
ForeverAnApple <kinbd8@gmail.com>
2026-05-23 17:36:20 -0700

    immich: nvenc transcode

--- modules/hosts/swordholder/immich.nix
+++ modules/hosts/swordholder/immich.nix
@@ -1,6 +1,15 @@
 {
   flake.modules.nixos."hosts/swordholder" = {
-    services.immich.mediaLocation = "/THICC/Immich";
+    services.immich = {
+      mediaLocation = "/THICC/Immich";
+      # NVENC for video transcode. UVM devices added separately when
+      # the CUDA machine-learning variant lands.
+      accelerationDevices = [
+        "/dev/nvidia0"
+        "/dev/nvidiactl"
+        "/dev/nvidia-modeset"
+      ];
+    };
 
     systemd.tmpfiles.rules = [
       "d /THICC/Immich 0700 immich immich -"
