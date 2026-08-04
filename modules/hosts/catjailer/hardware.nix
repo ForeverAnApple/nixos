c09458b6f438a632d9a6ccadd954060e26d3095e
ForeverAnApple <kinbd8@gmail.com>
2026-08-04 03:22:53 -0700

    catjailer: bound dirty pages, mq-deadline on disks

--- modules/hosts/catjailer/hardware.nix
+++ modules/hosts/catjailer/hardware.nix
@@ -29,7 +29,17 @@
 
         kernelModules = [ "kvm-amd" ];
         extraModulePackages = [ ];
+
+        # slow OEM SSD: cap flush backlog so write floods can't stall the desktop
+        kernel.sysctl = {
+          "vm.dirty_bytes" = 268435456;
+          "vm.dirty_background_bytes" = 134217728;
+        };
       };
+
+      services.udev.extraRules = ''
+        ACTION=="add|change", SUBSYSTEM=="block", KERNEL=="nvme*n*|sd*", ATTR{queue/scheduler}="mq-deadline"
+      '';
 
       networking.useDHCP = lib.mkDefault true;
 
