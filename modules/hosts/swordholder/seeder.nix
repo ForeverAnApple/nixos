cb47f2faeb031d3e2195edb1435cf36aab12cf81
ForeverAnApple <kinbd8@gmail.com>
2026-08-05 16:53:49 -0700

    seeder: qbittorrent 5.2.3

--- modules/hosts/swordholder/seeder.nix
+++ modules/hosts/swordholder/seeder.nix
@@ -53,7 +53,7 @@
               -v /var/lib/qbittorrent:/config \
               -v /media:/mnt/Plex \
               -v /THICC/THICC/Torrents:/mnt/Torrents \
-              lscr.io/linuxserver/qbittorrent:5.2.0
+              lscr.io/linuxserver/qbittorrent:5.2.3
           '';
           ExecStop = "${pkgs.docker}/bin/docker stop seeder";
           Restart = "on-failure";
