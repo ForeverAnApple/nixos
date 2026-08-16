4b3a5644ca756d2e254ae0eb79819fc70009b225
ForeverAnApple <kinbd8@gmail.com>
2026-08-16 00:33:28 -0700

    niri: fmt

--- modules/nixos/desktop/niri.nix
+++ modules/nixos/desktop/niri.nix
@@ -6,7 +6,9 @@
         # nixpkgs removed libdisplay-info_0_2; niri builds against 0.3 but
         # niri-flake still asserts version 0.2.0. Drop when upstream catches up.
         (final: prev: {
-          libdisplay-info_0_2 = final.libdisplay-info_0_3 // { version = "0.2.0"; };
+          libdisplay-info_0_2 = final.libdisplay-info_0_3 // {
+            version = "0.2.0";
+          };
         })
         inputs.niri.overlays.niri
       ];
