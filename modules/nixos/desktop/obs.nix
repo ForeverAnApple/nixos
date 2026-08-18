b6718a0491fc2456315f493c25c3241c5a63661e
ForeverAnApple <kinbd8@gmail.com>
2026-08-17 17:09:38 -0700

    obs: enable NVENC

--- modules/nixos/desktop/obs.nix
+++ modules/nixos/desktop/obs.nix
@@ -6,6 +6,7 @@
 
       programs.obs-studio = {
         enable = true;
+        package = pkgs.obs-studio.override { cudaSupport = true; };
         plugins = with pkgs.obs-studio-plugins; [ droidcam-obs ];
       };
     };
